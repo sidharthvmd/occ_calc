61b8f03a6873f6e671a9a8f46cd850bb2af569ce
sidharthvmd <75007892+sidharthvmd@users.noreply.github.com>
2022-09-24 21:11:41 +0530

    Drafted a generic UI and ...
    Added GUI and basic functionalities

--- occ.py
+++ occ.py
@@ -4,9 +4,64 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt
 
+#mainwindow
 class occMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("OCC")
+        self.setFixedSize(640,320)
+        self.parentlayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.parentlayout)
+        self.setCentralWidget(centralWidget)
+        self._display()
+        self._form()
+        self._add()
+        self._button()
+
+    def _display(self):
+        self.xdisplay = QLineEdit()
+        self.xdisplay.setReadOnly(True)
+        self.parentlayout.addWidget(self.xdisplay)
+        self.ydisplay = QLineEdit()
+        self.ydisplay.setReadOnly(True)
+        self.parentlayout.addWidget(self.ydisplay)
+
+    def _form(self):    
+        self.formlayout = QFormLayout()
+        self.x = QLineEdit()
+        self.y = QLineEdit()
+        self.x.setFixedWidth(50)
+        self.y.setFixedWidth(50)
+        self.formlayout.addRow("Ea value : ", self.x)
+        self.formlayout.addRow("If value : ",self.y)
+        self.parentlayout.addLayout(self.formlayout)
+
+    def _add(self):
+        self.add = QPushButton("Add")
+        self.parentlayout.addWidget(self.add)
+            
+    def _button(self):
+        self.submit = QPushButton("submit")
+        self.clear = QPushButton("clear")
+        self.buttonLayout = QHBoxLayout()
+        self.buttonLayout.addWidget(self.submit)
+        self.buttonLayout.addWidget(self.clear)
+        self.parentlayout.addLayout(self.buttonLayout)
+
+    def setDisplayText(self,x,y):
+        self.xdisplay.setText(x)
+        self.xdisplay.setFocus()
+        self.ydisplay.setText(y)
+        self.ydisplay.setFocus()
+
+    def displayText(self):
+        return self.xdisplay.x(), self.ydisplay.y()
+
+    def clearDisplaytext(self):
+        self.setDisplayText("","")
+
+
 
 #error dialog box
 class Error(QDialog):
@@ -23,12 +78,12 @@ class Error(QDialog):
         errorLayout.addLayout(errorFLayout)
         okayButton = QDialogButtonBox()
         okayButton.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
-        okayButton.clicked.connect(.close())
+        okayButton.clicked.connect(lambda:self.close())
         errorLayout.addWidget(okayButton)
         self.setLayout(errorLayout)
 
 if __name__ =="__main__":
-    window=QApplication([])
-    window=Error()
+    occ=QApplication([])
+    window=occMainWindow()
     window.show()
     sys.exit(occ.exec())
